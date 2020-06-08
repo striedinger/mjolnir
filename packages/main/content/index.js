98325b359ba6b42273d7a5609189bfdd936793c2
Hugo Striedinger <striedinger@Hugos-MacBook-Pro.local>
2020-06-08 18:33:38 -0400

    removed redux for move to context api

--- packages/main/content/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: {
-    type: 'stories'
-  },
-  bad: {
-    type: 'mock404'
-  }
-};
--- store/createStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
-
-export default (initialStore = {}) => {
-  const store = createStore(reducers, initialStore, applyMiddleware(thunk));
-  return store;
-};
