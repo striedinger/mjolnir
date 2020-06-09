f202c623dc8ddd07ccff23280ffa132875018114
Hugo Striedinger <striedinger@Hugos-MacBook-Pro.local>
2020-06-09 19:20:39 -0400

    code cleanup

--- components/feed/index.js
+++ components/feed/index.js
@@ -4,19 +4,17 @@ import useContent from '../../lib/shared/content';
 
 const Feed = () => {
   const [count, setCount] = useState(0);
-  const stories = useContent('stories', {});
-  console.log(stories);
+  const stories = useContent('stories', {}) || [];
   return (
     <div className={styles.feed}>
       <h2>Feed</h2>
       <button onClick={() => setCount(count+1)}>Click</button>
       <span>Count: {count}</span>
+      {stories.map((story, index) => {
+        return <p key={index}>{story.title}</p>
+      })}
     </div>
   );
-};
-
-export const loadData = () => {
-  console.log('loading Data');
 };
 
 export default Feed;
